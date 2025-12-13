ff58e8281aa81634894d22ddba756dd9dd834d7b
Stephen1902 <s_nichols_other@yahoo.co.uk>
2025-12-13 16:11:19 +0000

    Initial Commit

---
+++ Source/CoOpEscape/CoOpEscape.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoOpEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoOpEscape, "CoOpEscape" );
+ 
